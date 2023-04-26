33cdd8799a6c9c37a91e894c0008fbe44bd133a8
Yannik Hangst <yanso1@posteo.de>
2023-04-26 17:24:02 +0200

    add testing

---
+++ frontend/src/tests/Header.test.jsx
@@ -0,0 +1,22 @@
+import { expect } from "vitest";
+import Header from "../components/Header/Header";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("test header", () => {
+  it("should display the h1 correctly", () => {
+    render(<Header />);
+    const headline = screen.getByText("mood");
+    expect(headline).toBeVisible();
+  });
+  it("should render boards correctly", () => {
+    render(<Header />);
+    const navBoards = screen.getByText("Boards");
+    expect(navBoards).toBeInTheDocument();
+  });
+  it("should render login/profile correctly", () => {
+    render(<Header />);
+    const navLogin = screen.getByText("Login/Profile");
+    expect(navLogin).toBeInTheDocument();
+  });
+});
