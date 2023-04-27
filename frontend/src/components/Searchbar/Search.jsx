2d3c6b980298a4fa7c217d8233eb9fe06712d855
Yannik Hangst <yanso1@posteo.de>
2023-04-27 12:35:37 +0200

    set svg to button

--- frontend/src/components/Searchbar/Search.jsx
+++ frontend/src/components/Searchbar/Search.jsx
@@ -5,10 +5,25 @@ const Search = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Find your next idea</h1>
-          <form className="flex-col">
-            <label htmlFor="searchBar">Search</label>
+          <form className="flex justify-center input-group">
+            <label htmlFor="searchBar"></label>
             <input type="text" className="input input-bordered" />
-            <button className="btn btn-primary">Get Started</button>
+            <button class="btn btn-square btn-primary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
           </form>
         </div>
       </div>
---
+++ frontend/src/components/Searchbar/Search.test.jsx
@@ -0,0 +1,4 @@
+import { expect } from "vitest";
+import Search from "./Search";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
