917532b87566d5e6d4e98fb699891dfacda8b990
Yannik Hangst <yanso1@posteo.de>
2023-04-29 16:31:08 +0200

    add link to pexels

--- frontend/src/components/SearchResults/SearchResults.jsx
+++ frontend/src/components/SearchResults/SearchResults.jsx
@@ -1,12 +1,18 @@
 const SearchResults = ({ searchResults }) => {
   return (
-    <div>
-      <h2 className="text-center">Results</h2>
-      <div className="grid grid-cols-4 gap-4">
+    <div className="p-8">
+      <div className="grid grid-cols-4 gap-8">
         {searchResults.map((searchResult) => {
           return (
             <div key={searchResult.id}>
               <img src={searchResult.src.tiny} />
+              <span>
+                This <a href={searchResult.url}>Photo</a> was taken by
+                <a href={searchResult.photographer_url}>
+                  {searchResult.photographer}
+                </a>
+                on Pexels.
+              </span>
             </div>
           );
         })}
