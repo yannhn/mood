731b3090a3b87dafa8cbb04449a9f32ca1baefac
Yannik Hangst <yanso1@posteo.de>
2023-05-02 13:21:36 +0200

    setup boards page

--- frontend/src/routes/Boards.jsx
+++ frontend/src/routes/Boards.jsx
@@ -1,7 +1,11 @@
 import Header from "../components/Header/Header";
 
 const Boards = () => {
-  return <></>;
+  return (
+    <>
+      <h2 className="text-center text-6xl mt-8">Your boards</h2>
+    </>
+  );
 };
 
 export default Boards;
